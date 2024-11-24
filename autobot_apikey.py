c16a6226fd02a1e599e7625db80505598600cbe0
hasangsu <ssha@autocrypt.io>
2024-11-24 10:01:13 +0900

    delete api kiy

--- autobot_apikey.py
+++
@@ -1,6 +0,0 @@
-# slack api webhooks
-SLACK_HOOKS_URL = "https://hooks.slack.com/services/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"
-
-# upbit api keys
-ACCESS_KEY = "P4oH9bjAqXHQ2JbyTkogKkbrGR0BNPQXvWpUI8Ml"
-SECRET_KEY = "craU0SgR68iZBFdwZ1bKy9UxwWY7x2phu12kAOtf"
